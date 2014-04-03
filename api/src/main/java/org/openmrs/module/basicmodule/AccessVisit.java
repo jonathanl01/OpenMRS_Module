79a72b294f027168a2a4103b2d31f4b25af955fd
Jonathan <jonathanlei123@hotmail.com>
2014-04-03 14:28:47 -0700

    changed method names in AccessVisit and added default constructor

--- api/src/main/java/org/openmrs/module/basicmodule/AccessVisit.java
+++ api/src/main/java/org/openmrs/module/basicmodule/AccessVisit.java
@@ -14,6 +14,7 @@ import java.sql.Date;
  */
 public class AccessVisit {
     
+    protected Integer id;
     protected Date date;
     protected char viewed_edited;
     protected Integer visit_id;
@@ -22,8 +23,13 @@ public class AccessVisit {
     protected Integer location_id;
     
     //Default constructor
-    public AccessVisit(Date date, char viewed_edited, Integer visit_id,
+    public AccessVisit(){
+        
+    }
+    
+    public AccessVisit(Integer id, Date date, char viewed_edited, Integer visit_id,
             Integer user_id, Integer patient_id, Integer location_id){
+        this.id = id;
         this.date = date;
         this.viewed_edited = viewed_edited;
         this.visit_id = visit_id;
@@ -32,27 +38,55 @@ public class AccessVisit {
         this.location_id = location_id;
     }
     
+    public Integer getId(){
+        return id;
+    }
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    
     public Date getDate(){
         return date;
     }
     
-    public char getViewedEdited(){
+    public void setDate(Date date){
+        this.date = date;
+    }
+    
+    public char getViewed_edited(){
         return viewed_edited;
     }
     
-    public Integer getVisitID(){
+    public void setViewed_edited(char viewed_edited){
+        this.viewed_edited = viewed_edited;
+    }
+    
+    public Integer getVisit_id(){
         return visit_id;
     }
     
-    public Integer getUserID(){
+    public void setVisit_id(Integer visit_id){
+        this.visit_id = visit_id;
+    }
+    
+    public Integer getUser_id(){
         return user_id;
     }
     
-    public Integer getPatientID(){
+    public void setUser_id(Integer user_id){
+        this.user_id = user_id;
+    }
+    
+    public Integer getPatient_id(){
         return patient_id;
     }
     
-    public Integer getLocationID(){
+    public void setPatient_id(Integer patient_id){
+        this.patient_id = patient_id;
+    }
+    
+    public Integer getLocation_id(){
         return location_id;
     }
 }
